81bdda6396861ccee8eab65034830445ac4ffbab
Brendan <brndnmcmns@gmail.com>
2015-01-16 18:25:31 -0500

    Initial Commit

---
+++ emoteful/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emoteful
+//
+//  Created by Brendan McManus on 1/16/15.
+//  Copyright (c) 2015 Brendan McManus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
